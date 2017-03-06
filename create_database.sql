4fd26707becf54b098e760447f3009153cce7a41
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-06 14:38:06 -0600

    Databse created

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,13 @@
 -- Database name
-
+treats
 -- Document your create tables SQL here
+CREATE TABLE treats(
+id SERIAL PRIMARY KEY,
+treat_name VARCHAR(40) NOT NULL,
+description VARCHAR(260) NOT NULL,
+image_path VARCHAR(260) NOT NULL);
+
+INSERT INTO treats (treat_name, description, image_path)
+VALUES
+('Cupcake', 'A delicious cupcake', '/assets/cupcake.jpg'),
+('Donuts', 'Mmmm donuts', '/assets/donuts.jpg');
