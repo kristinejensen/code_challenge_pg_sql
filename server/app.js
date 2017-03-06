be5efafb7339e2a61f94f20a03bec1f991988641
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-06 14:48:38 -0600

    building route

--- server/app.js
+++ server/app.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+var bodyParser = require('body-parser');
+var treats = require('./routes/treats');
 var port = process.env.PORT || 3000;
-
-/*** Build out a module to manage our treats requests. ***/
-
-
-
 
 // Get static files
 app.use(express.static('./server/public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Get index.html
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('./server/public/views/index.html'));
 });
 
+app.use('/treats', treats);
 
 app.listen(port, function() {
   console.log('Server running on port: ', port);
---
+++ server/routes/treats.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var config = {
+  database: 'phi',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
+};
